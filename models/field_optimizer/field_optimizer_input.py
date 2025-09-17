2338034c33218dcdf9191938ab76a8ffcc8671c4
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-09-17 18:21:02 +0200

    Add field optimizer input model and test examples

---
+++ models/field_optimizer/field_optimizer_input.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class Field(BaseModel):
+    name: str
+    size: int
+    unavailable_start_times: list[int]
+
+
+class Group(BaseModel):
+    name: str
+    minimum_number_of_activities: int
+    maximum_number_of_activities: int
+    possible_start_times: list[int]
+    preferred_start_times: list[int]
+    preferred_start_time_activity_1: int
+    preferred_start_time_activity_2: int
+    size_required: int
+    duration: int
+    priority: int
+
+
+class FieldOptimizerInput(BaseModel):
+    fields: list[Field]
+    groups: list[Group]
+    time_slots: list[int]
