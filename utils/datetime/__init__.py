a4cc0e801d9f7bca3ae1c01f696e66517f272ea6
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-10-15 20:02:12 +0200

    Add datetime utility functions
    Introduced a new module with utility functions for time manipulation, including adding minutes to a time string, converting a time string to a datetime object, and checking if a time is between two other times. This enhances the application's datetime handling capabilities.

---
+++ utils/datetime/__init__.py
@@ -0,0 +1,9 @@
+from utils.datetime.add_minutes_to_time_string import add_minutes_to_time_string
+from utils.datetime.convert_time_to_datetime import convert_time_to_datetime
+from utils.datetime.is_time_between import is_time_between
+
+__all__ = [
+    "add_minutes_to_time_string",
+    "convert_time_to_datetime",
+    "is_time_between",
+]
