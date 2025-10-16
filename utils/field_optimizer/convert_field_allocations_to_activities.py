1f5da050a088a51c1726080239dbc71f0f6aa16f
Samuel Hellstrøm <samuel@easyplay.no>
2025-10-16 17:05:57 +0200

    Update requirements and add tests for field allocation conversion
    Enhanced the conversion function to handle day boundaries when grouping timeslots into activities.

--- utils/field_optimizer/convert_field_allocations_to_activities.py
+++ utils/field_optimizer/convert_field_allocations_to_activities.py
@@ -2,9 +2,13 @@ from models.field_optimizer.field_allocation import FieldAllocation
 from models.field_optimizer.field_activity import FieldActivity
 
 
-# TODO:
-# - Make sure timeslotIds to an activity is within same day,
-# - else stop look for next timeslot
+def get_day_for_timeslot(timeslot_id: int, timeslot_ids: list[list[int]]) -> int:
+    """Find which day (index) a timeslot belongs to."""
+    for day_idx, day_timeslots in enumerate(timeslot_ids):
+        if timeslot_id in day_timeslots:
+            return day_idx
+    return -1  # Not found
+
 
 def convert_field_allocations_to_activities(
     field_allocations: list[FieldAllocation],
@@ -13,9 +17,11 @@ def convert_field_allocations_to_activities(
     """
     Convert field allocations to activities by grouping consecutive timeslots
     for the same group on the same field into activity blocks.
+    Activities are split at day boundaries (when timeslots are not on the same day).
 
     Args:
         field_allocations: List of FieldAllocation objects
+        timeslot_ids: List of lists, where each sublist represents timeslots for a day
 
     Returns:
         List of FieldActivity objects representing grouped activities
@@ -48,8 +54,33 @@ def convert_field_allocations_to_activities(
         elif (current_activity['field'] == field and
               current_activity['group'] == group and
               timeslot_id == current_activity['end_timeslot'] + 1):
-            # Extend current block (consecutive timeslot)
-            current_activity['end_timeslot'] = timeslot_id
+            # Check if this consecutive timeslot is on the same day
+            current_day = get_day_for_timeslot(
+                current_activity['end_timeslot'], timeslot_ids)
+            new_day = get_day_for_timeslot(timeslot_id, timeslot_ids)
+
+            if current_day == new_day:
+                # Extend current block (consecutive timeslot on same day)
+                current_activity['end_timeslot'] = timeslot_id
+            else:
+                # Finish current block and start a new one (crossed day boundary)
+                activities.append(FieldActivity(
+                    field=current_activity['field'],
+                    group=current_activity['group'],
+                    start_timeslot=current_activity['start_timeslot'],
+                    end_timeslot=current_activity['end_timeslot'],
+                    duration=current_activity['end_timeslot'] -
+                    current_activity['start_timeslot'] + 1,
+                    size=current_activity['size']
+                ))
+
+                current_activity = {
+                    'field': field,
+                    'group': group,
+                    'start_timeslot': timeslot_id,
+                    'end_timeslot': timeslot_id,
+                    'size': size
+                }
         else:
             # Finish current block and start a new one
             activities.append(FieldActivity(
