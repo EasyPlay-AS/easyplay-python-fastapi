27dc18aedda75c343dda3ce02f1c3ec7b9bc5720
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-10-27 07:59:12 +0100

    `convert_time_range_to_timeslot_ids` function to map time ranges to timeslot IDs - with tests

--- utils/field_optimizer/__init__.py
+++ utils/field_optimizer/__init__.py
@@ -10,10 +10,14 @@ from utils.field_optimizer.convert_field_allocations_to_activities import (
 from utils.field_optimizer.convert_payload_to_input import (
     convert_payload_to_input
 )
+from utils.field_optimizer.convert_time_range_to_timeslot_ids import (
+    convert_time_range_to_timeslot_ids
+)
 
 __all__ = [
     "convert_ampl_x_values_to_allocations",
     "convert_field_activities_to_result",
     "convert_field_allocations_to_activities",
     "convert_payload_to_input",
+    "convert_time_range_to_timeslot_ids",
 ]
